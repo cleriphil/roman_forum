d5963edd0b124796b07ca6682c13c7e2d8802f5e
cleriphil <cleri.phil@gmail.com>
2015-06-12 13:54:16 -0700

    Add styling for Roman Forum

--- router.js
+++ router.js
@@ -4,4 +4,5 @@ MessageBoard.Router.map(function() {
   this.resource('question', {path: 'question/:question_id'}, function(){
     this.resource('new-answer');
   });
+  this.resource('answer', {path: 'answer/:answer_id'});
 });
