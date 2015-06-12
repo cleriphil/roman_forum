b8850b3b56c4159113e9fc5bdd206bed9f266a40
cleriphil <cleri.phil@gmail.com>
2015-06-12 12:21:10 -0700

    Add answer to question

--- controllers/new-answer.js
+++ controllers/new-answer.js
@@ -1,22 +1,24 @@
 MessageBoard.NewAnswerController = Ember.Controller.extend({
   needs: ['question'],
   actions: {
-    save: function {
+    save: function() {
       var answer = this.store.createRecord('answer', {
         answer: this.get('answer'),
-        author: this.get('author')
+        author: this.get('author'),
       });
 
       answer.save();
 
-      var question = this.get('controllers.lot.model');
+      var question = this.get('controllers.question.model');
       question.get('answers').pushObject(answer);
       question.save();
 
       this.set('answer', '');
       this.set('author', '');
 
-     this.transitionToRoute('lot', lot.id);
+      this.transitionToRoute('questions');
+
+     this.transitionToRoute('question', question.id);
     }
   }
-});
+})
